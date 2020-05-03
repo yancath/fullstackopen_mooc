dbbd7138c8fdca76c1f658c9742c9909c91e6737
yancath <catherine.yan01@gmail.com>
2020-05-03 02:43:26 -0400

    1.3 submission

---
+++ part1/1.3/src/index.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.course}</h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+  <p>
+    {props.part} {props.exercise}
+  </p>
+  )
+}
+
+const Content = (props) => (
+  <div>
+      <Part part={props.part1.name} exercise={props.part1.exercises}/>
+      <Part part={props.part2.name} exercise={props.part2.exercises}/>
+      <Part part={props.part3.name} exercise={props.part3.exercises}/>
+  </div>
+)
+
+const Total = (props) => (
+  <div>
+    <p>Number of exercises {props.part1.exercises + props.part2.exercises + props.part3.exercises}</p>
+  </div>
+)
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part1={part1} part2={part2} part3={part3} />
+      <Total part1={part1} part2={part2} part3={part3}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
