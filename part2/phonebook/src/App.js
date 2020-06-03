5636c0ff6c7a472846434ac5e3f03f842b6b2baa
yancath <catherine.yan01@gmail.com>
2020-06-03 16:36:18 -0400

    Added 2.17 & 2.18 submission

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -6,6 +6,8 @@ import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 import Filter from './components/Filter'
 
+import personsService from './services/Persons'
+
 const App = () => {
   const [persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
@@ -29,16 +31,32 @@ const App = () => {
     const everyPerson = persons.map(person => person.name);
 
     if (everyPerson.includes(newName)) {
-      alert(`${newName} is already added to phonebook`);
+      if (window.confirm(`${newName} is already added to phonebook, replace old number with a new one?`)) {
+        const id = persons.find(person => person.name === newName).id
+
+        const newObj = {
+          name: newName,
+					number: newNumber,
+        }
+
+        personsService
+        .update(id, newObj)
+        .then(returnedPerson => {
+          setPersons(persons.map(person => person.id === id ? returnedPerson : person))
+        })
+
+      }
+    } else {
+      personsService
+      .create(personObj)
+      .then(returnedPerson => {
+        setPersons(persons.concat(returnedPerson))
+        setNewName('');
+        setNewNumber('');
+      })
     }
 
-    axios
-    .post('http://localhost:3001/persons', personObj)
-    .then(response => {
-      setPersons(persons.concat(personObj));
-      setNewName('');
-      setNewNumber('');
-    })
+
   }
 
   const showFilter = filter === ''
@@ -58,8 +76,20 @@ const App = () => {
     setFilter(event.target.value)
   }
 
+  const delHandler = (id) => {
+    const person = persons.find(person => person.id === id)
+    
+    if (window.confirm(`Delete ${person.name}?`) === true) {
+      personsService
+      .remove(id)
+      .then(response => {
+          setPersons(persons.filter(person => person.id !== id))                
+      })
+    }
+  }
+
   const displayNames = () => 
-    showFilter.map(person => <Person key={person.name} persons={person}/>)
+    showFilter.map(person => <Person key={person.id} persons={person} delHandler={() => delHandler(person.id)}/>)
   
 
   return (
@@ -74,7 +104,9 @@ const App = () => {
       handlePersonChange={handlePersonChange} 
       handleNumChange={handleNumChange}/>
       <h3>Numbers</h3>
-        <Persons persons={displayNames()}/>
+        <Persons 
+        persons={displayNames()}
+        />
     </div>
 
   )
--- part2/phonebook/src/components/Person.js
+++ part2/phonebook/src/components/Person.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
-const Person = ( {persons} ) => {
+const Person = ( {persons, delHandler} ) => {
     return (
-    <li>{persons.name} {persons.number}</li>
+    <li>{persons.name} {persons.number} <button onClick={delHandler}>delete</button></li>
     )
 }
 
