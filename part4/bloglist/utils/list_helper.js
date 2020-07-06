0dc32f9d87d0acdb69e2e82ac57a713d40e96418
yancath <catherine.yan01@gmail.com>
2020-07-05 23:06:38 -0400

    Submission for 4.4 added

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -3,7 +3,15 @@ const dummy = (blogs) => {
         return 1;
     }
   }
+
+const totalLikes = (blogs) => {
+  const sumUp = (sum, {likes}) => {
+    return sum + likes
+  }
+
+  return blogs.reduce(sumUp, 0)
+}
   
   module.exports = {
-    dummy
+    dummy, totalLikes
   }
