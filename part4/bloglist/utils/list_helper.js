ee4605f1778124b41d6630fc9b94a25de4b74f53
yancath <catherine.yan01@gmail.com>
2020-07-05 23:47:30 -0400

    Submission for 4.5 added

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -11,7 +11,23 @@ const totalLikes = (blogs) => {
 
   return blogs.reduce(sumUp, 0)
 }
+
+const favoriteBlog = (blogs) => {
+  max = 0
+
+  if (blogs.length === 0) {
+    return 0;
+  }
+
+  for (i = 0; i < blogs.length; i++) {
+    if (blogs[i].likes > max) {
+      max = blogs[i].likes
+    }
+  }
+
+  return max;
+}
   
   module.exports = {
-    dummy, totalLikes
+    dummy, totalLikes, favoriteBlog
   }
